d266f5788c3927d9a6f85e41f1fd4206b2ae8f5e
SilverFox70 <wfbrinkert@gmail.com>
2016-05-10 14:47:10 -0700

    Fix syntax error line 6 place.rb

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,5 +3,5 @@ class Place < ActiveRecord::Base
   has_many :open_times
   has_many :bookings
   has_many :reviews
-  belongs_to :user, foreign_key :owner_id
+  belongs_to :user, foreign_key: :owner_id
 end
