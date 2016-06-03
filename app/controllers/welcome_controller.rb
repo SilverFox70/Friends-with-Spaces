19584af656d786abb5df95c799001caed07d2f2e
SilverFox70 <wfbrinkert@gmail.com>
2016-06-02 17:07:31 -0700

    Add index method to welcome controller;redirect on user sign in not working; bug with finding user sign_out; added line 7 debug code to show place view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,9 @@
 class WelcomeController < ApplicationController
+	def index
+    if current_user
+      @place = Place.where(["owner_id = ?",current_user.id])
+    else
+      render :index
+    end
+  end
 end
