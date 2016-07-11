533672c9b019bd1a16b7733891e06de773ff7f6e
SilverFox70 <wfbrinkert@gmail.com>
2016-07-11 16:41:13 -0700

    Fix nested pictures resources and routes

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -3,6 +3,12 @@ class PicturesController < InheritedResources::Base
 	def index
     @place = Place.find(params[:place_id])
     @pictures = @place.pictures.all
+	end
+
+  def edit
+    @place = Place.find_by_id(params[:place_id])
+    @picture = Picture.find_by_id(params[:id])
+    render "edit"
   end
 
   private
